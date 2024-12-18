b4cf41d915e0a2e0b5dd527909613304b1e8513a
Caleb <kalebotchere@gmail.com>
2024-12-18 22:55:18 +0000

    Update User model to make fullName optional

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    fullName: { type: String, required: true },
+    fullName: { type: String },
     phone: { type: String },
     location: { type: String },
     preferences: {
